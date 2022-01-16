37a952a3a4baeaa9c6e909b02b3263968fb6eb42
James Moore <james816am@gmail.com>
2022-01-16 13:56:53 -0500

    Homepage (not-logged-in) all done, I think...

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./routes/Home";
+// import Search from "./routes/Search";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/search" element={<Search />} /> */}
       </Routes>
     </Router>
   );
