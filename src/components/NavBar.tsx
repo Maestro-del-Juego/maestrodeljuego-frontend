a8f5b671863caae90f07b14cd069a0c557537ce1
TrentHurlbut <trent.hurlbut@gmail.com>
2022-01-18 08:00:36 -0800

    Prepping for merge.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,22 +2,22 @@ import { Link } from 'react-router-dom';
 import image from '../assets/logo.png';
 
 interface navProps {
-<<<<<<< HEAD
-  user: string,
-  logout: any,
-  avatar: string
-=======
   user: string;
   logout: any;
->>>>>>> main
+  avatar: string;
 }
 
 export default function NavBar(props: navProps) {
   return (
     <>
       <div id="nav-bar">
-        <Link to={(props.user === '') ? "/" : `/play_stats/${props.user}` }>
-          <img src={image} id="logo" title="Game Master" alt="Game Master Logo" />
+        <Link to={props.user === '' ? '/' : `/play_stats/${props.user}`}>
+          <img
+            src={image}
+            id="logo"
+            title="Game Master"
+            alt="Game Master Logo"
+          />
         </Link>
         <div id="site-nav">
           <Link to="/">Your Collection</Link> |<Link to="/about">Wishlist</Link>{' '}
@@ -29,11 +29,19 @@ export default function NavBar(props: navProps) {
             <>
               <Link to="/login">Login</Link>|
               <Link to="/registration">Register</Link>
-            </>)
-            :
-            (<div id="navbar-avatar">
+            </>
+          ) : (
+            <div id="navbar-avatar">
               <Link to="/user_page">
-                <img src={(props.avatar === '' || props.avatar === null) ? "https://thumbs.dreamstime.com/b/vector-illustration-isolated-white-background-user-profile-avatar-black-line-icon-user-profile-avatar-black-solid-icon-121102166.jpg" : props.avatar} title="User Avatar" id="avatar" />
+                <img
+                  src={
+                    props.avatar === '' || props.avatar === null
+                      ? 'https://thumbs.dreamstime.com/b/vector-illustration-isolated-white-background-user-profile-avatar-black-line-icon-user-profile-avatar-black-solid-icon-121102166.jpg'
+                      : props.avatar
+                  }
+                  title="User Avatar"
+                  id="avatar"
+                />
               </Link>
               <Link to="/" onClick={props.logout} id="log-out">
                 Log Out
