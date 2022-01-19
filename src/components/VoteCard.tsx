0fb0ea9f78c4d8915ca74f6975a6e15d2a291c55
TrentHurlbut <trent.hurlbut@gmail.com>
2022-01-19 08:42:21 -0800

    Game night vote form skeleton in place.

--- src/components/VoteCard.tsx
+++ src/components/VoteCard.tsx
@@ -2,12 +2,23 @@ export default function VoteCard() {
   return (
     <div id="vote-card-container">
       <h3>Game Title</h3>
-      <img src='https://m.media-amazon.com/images/I/9172ZauQ7CL._AC_SY550_.jpg' alt='Game Cover Art' />
+      <img
+        src="https://m.media-amazon.com/images/I/9172ZauQ7CL._AC_SY550_.jpg"
+        alt="Game Cover Art"
+        className="vote-card-image"
+      />
       <div id="vote-buttons">
-        <button id="smiley-vote" title="smiley-vote"><i className="far fa-smile"></i></button>
-        <button id="neutral-vote" title="neutral-vote"><i className="far fa-meh"></i></button>
-        <button id="frowny-vote" title="frowny-vote"><i className="far fa-frown"></i></button>«
+        <button className="smiley-vote" title="smiley-vote">
+          <i className="far fa-smile"></i>
+        </button>
+        <button className="neutral-vote" title="neutral-vote">
+          <i className="far fa-meh"></i>
+        </button>
+        <button className="frowny-vote" title="frowny-vote">
+          <i className="far fa-frown"></i>
+        </button>
+        «
       </div>
     </div>
-  )
+  );
 }
--- src/routes/VotingForm.tsx
+++ src/routes/VotingForm.tsx
@@ -1,44 +1,62 @@
 import { useState } from 'react';
 import VoteCard from '../components/VoteCard';
 
-const dummyGuests = ['Frodo', 'Merry', 'Sam', 'Pippin', 'Aragorn', 'Boromir', 'Legolas', 'Gimli', 'Gandalf']
+const dummyGuests = [
+  'Frodo',
+  'Merry',
+  'Sam',
+  'Pippin',
+  'Aragorn',
+  'Boromir',
+  'Legolas',
+  'Gimli',
+  'Gandalf',
+];
 
 export default function VotingForm() {
-
   const [guestPick, setGuestPick] = useState(true);
 
   const guestListHandler = () => {
-    setGuestPick(false)
-  }
+    setGuestPick(!guestPick);
+  };
 
   let gameArray = [];
 
-  for (let i = 0; i < 5; i++){
-    gameArray.push(<VoteCard />)
+  for (let i = 0; i < 5; i++) {
+    gameArray.push(<VoteCard />);
   }
 
-  return (
-    guestPick ? (
-      <div
-        id="guest-list-select">
-        <form onSubmit={guestListHandler}>
-          <label htmlFor="guest-list-dropdown">
-          <select title="Guest List Dropdown" name="guests" id="guest-list-dropdown">
-              {dummyGuests.map((name, i) => {
-                <option value={name} key={i}>{name}</option>
-              })}
+  return guestPick ? (
+    <div id="guest-list-select">
+      <form onSubmit={guestListHandler} id="guest-list-form">
+        <h3>Guest List</h3>
+        <div id="guest-select">
+          <select
+            title="Guest List Dropdown"
+            name="guests"
+            id="guest-list-dropdown"
+          >
+            {dummyGuests.map((name, i) => {
+              return (
+                <option value={name} key={i}>
+                  {name}
+                </option>
+              );
+            })}
           </select>
-          </label>
-          <button type="submit">Select</button>
-        </form>
-      </div>)
-      :
-      (<div id="guest-vote-form">
-        <h1>Welcome to Game Night!</h1>
-        <div id="game-suggestions">
-          {gameArray}
+          <button type="submit" id="guest-list-button">
+            Select
+          </button>
         </div>
-      </div>
-      )
-  )
+      </form>
+    </div>
+  ) : (
+    <div id="guest-vote-form">
+      <h1 id="vote-header">Welcome to Game Night!</h1>
+      <div id="game-suggestions">{gameArray}</div>
+      <button type="submit" onClick={guestListHandler} id="vote-submit">
+        Back
+      </button>
+    </div>
+  );
 }
