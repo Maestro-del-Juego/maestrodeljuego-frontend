70c48682f4433016cf0f5c7af3473bbeddd9d0d3
TrentHurlbut <trent.hurlbut@gmail.com>
2022-01-24 14:00:20 -0800

    Vote buttons now highlight when selected. All working properly.

--- src/components/VoteCard.tsx
+++ src/components/VoteCard.tsx
@@ -1,3 +1,6 @@
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+
 interface voteCardProps {
   url: string;
   title: string;
@@ -6,32 +9,90 @@ interface voteCardProps {
 }
 
 export default function VoteCard(props: voteCardProps) {
+  const [smileySelected, setSmileySelected] = useState(false);
+  const [neutralSelected, setNeutralSelected] = useState(false);
+  const [frownySelected, setFrownySelected] = useState(false);
+
   return (
     <div id="vote-card">
       <h5>{props.title}</h5>
       <img src={props.url} alt="Game Cover Art" className="vote-card-image" />
       <div id="vote-buttons">
-        <button
-          className="smiley-vote"
-          title="smiley-vote"
-          onClick={() => props.voteHandler(props.pk, 1)}
-        >
-          <i className="far fa-smile"></i>
-        </button>
-        <button
-          className="neutral-vote"
-          title="neutral-vote"
-          onClick={() => props.voteHandler(props.pk, 0)}
-        >
-          <i className="far fa-meh"></i>
-        </button>
-        <button
-          className="frowny-vote"
-          title="frowny-vote"
-          onClick={() => props.voteHandler(props.pk, -1)}
-        >
-          <i className="far fa-frown"></i>
-        </button>
+        {smileySelected ? (
+          <Button
+            className="smiley-vote"
+            title="smiley-vote"
+            variant="success"
+            onClick={() => {
+              props.voteHandler(props.pk, 0);
+              setSmileySelected(false);
+            }}
+          >
+            <i className="far fa-smile"></i>
+          </Button>
+        ) : (
+          <Button
+            className="smiley-vote"
+            title="smiley-vote"
+            variant="outline-success"
+            onClick={() => {
+              props.voteHandler(props.pk, 1);
+              setSmileySelected(true);
+            }}
+          >
+            <i className="far fa-smile"></i>
+          </Button>
+        )}
+        {neutralSelected ? (
+          <Button
+            className="smiley-vote"
+            title="smiley-vote"
+            variant="warning"
+            onClick={() => {
+              props.voteHandler(props.pk, 0);
+              setNeutralSelected(false);
+            }}
+          >
+            <i className="far fa-meh"></i>
+          </Button>
+        ) : (
+          <Button
+            className="smiley-vote"
+            title="smiley-vote"
+            variant="outline-warning"
+            onClick={() => {
+              props.voteHandler(props.pk, 0);
+              setNeutralSelected(true);
+            }}
+          >
+            <i className="far fa-meh"></i>
+          </Button>
+        )}
+        {frownySelected ? (
+          <Button
+            className="smiley-vote"
+            title="smiley-vote"
+            variant="danger"
+            onClick={() => {
+              props.voteHandler(props.pk, 0);
+              setFrownySelected(false);
+            }}
+          >
+            <i className="far fa-frown"></i>
+          </Button>
+        ) : (
+          <Button
+            className="smiley-vote"
+            title="smiley-vote"
+            variant="outline-danger"
+            onClick={() => {
+              props.voteHandler(props.pk, -1);
+              setFrownySelected(true);
+            }}
+          >
+            <i className="far fa-frown"></i>
+          </Button>
+        )}
       </div>
     </div>
   );
