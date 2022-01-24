8b50f680598f545a8030b29f901ec74c15cb4998
TrentHurlbut <trent.hurlbut@gmail.com>
2022-01-24 08:35:29 -0800

    On paper, a fully functional game night flow for invitees.

--- src/routes/VotingForm.tsx
+++ src/routes/VotingForm.tsx
@@ -3,6 +3,7 @@ import VoteCard from '../components/VoteCard';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Button } from 'react-bootstrap';
+import GameCard from '../components/GameCard';
 interface gameNightProps {
   token: string;
 }
@@ -15,7 +16,14 @@ export default function VotingForm(props: gameNightProps) {
   const [guest, setGuest] = useState('');
   const [voter, setVoter] = useState(0);
   const [votes, setVotes] = useState<any[]>([]);
-  const [attending, setAttending] = useState(true);
+  const [attending, setAttending] = useState('True');
+  const [inviteeEmail, setInviteeEmail] = useState('');
+  const [eventStatus, setEventStatus] = useState('');
+  const [voted, setVoted] = useState(false);
+  const [emailError, setEmailError] = useState('');
+  const [finalGames, setFinalGames] = useState<any[]>([]);
+  const [gameNightDate, setGameNightDate] = useState('');
+  const [gameNightLoc, setGameNightLoc] = useState('');
 
   let { gameId } = useParams();
 
@@ -29,11 +37,16 @@ export default function VotingForm(props: gameNightProps) {
         setGameNightPk(result.data.pk);
         console.log(result.data.options);
         setGameList(result.data.options);
+        setEventStatus(result.data.status);
+        setFinalGames(result.data.games);
+        setGameNightDate(result.data.date);
+        setGameNightLoc(result.data.location);
         setGuest(
           result.data.invitees[0].first_name +
             ' ' +
             result.data.invitees[0].last_name
         );
+        setVoter(result.data.invitees[0].pk);
         for (let i = 0; i < result.data.options.length; i++) {
           setVotes((oldData) => [
             ...oldData,
@@ -47,13 +60,53 @@ export default function VotingForm(props: gameNightProps) {
       .catch((error: any) => console.log(error));
   }, []);
 
-  const guestListHandler = () => {
-    setGuestPick(!guestPick);
+  useEffect(() => {
     for (let individual of guestList) {
       if (individual.first_name + ' ' + individual.last_name === guest) {
         setVoter(individual.pk);
       }
     }
+  }, [guest]);
+
+  const rsvpHandler = (event: any) => {
+    event.preventDefault();
+    let rsvpFirstName = '';
+    let rsvpLastName = '';
+    for (let individual of guestList) {
+      if (individual.pk === voter) {
+        rsvpFirstName = individual.first_name;
+        rsvpLastName = individual.last_name;
+      }
+    }
+    let rsvpSubmission = {
+      invitee: {
+        first_name: rsvpFirstName,
+        last_name: rsvpLastName,
+        email: inviteeEmail,
+      },
+      attending: attending,
+    };
+    console.log(rsvpSubmission);
+    axios
+      .post(
+        `https://maestrodeljuego.herokuapp.com/gamenight/${gameId}/RSVP/`,
+        rsvpSubmission,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        setGuestPick(!guestPick);
+      })
+      .catch((error) => {
+        console.log(error);
+        setEmailError(
+          'The email you entered did not match the record we have for this guest.'
+        );
+      });
   };
 
   const voteHandler = (gamePk: number, voteScore: number) => {
@@ -88,9 +141,38 @@ export default function VotingForm(props: gameNightProps) {
       .catch((error) => console.log(error));
   };
 
-  return guestPick ? (
+  return eventStatus === 'Cancelled' ? (
+    <h1 className="gamenight-banner">
+      I'm sorry, your host has cancelled this event!
+    </h1>
+  ) : voted ? (
+    <>
+      <h1 className="gamenight-banner">Thank you for feedback!</h1>
+      <h4>Your host will contact you when the event is finalized.</h4>
+    </>
+  ) : eventStatus === 'Finalized' ? (
+    <div id="finalized-event">
+      <h1 className="gamenight-banner">Your game night is all set!</h1>
+      <h3>Here are the games you'll be playing:</h3>
+      <div id="final-game-container">
+        {finalGames.map((game: any, i: any) => {
+          return (
+            <GameCard
+              gameId={game.bgg}
+              gameName={game.title}
+              pubYear={game.pub_year}
+              imageUrl={game.image}
+            />
+          );
+        })}
+      </div>
+      <h2>
+        This game night will take place on {gameNightDate} at {gameNightLoc}.
+      </h2>
+    </div>
+  ) : guestPick ? (
     <div id="guest-list-select">
-      <form onSubmit={guestListHandler} id="guest-list-form">
+      <form onSubmit={rsvpHandler} id="guest-list-form">
         <h3>Guest List</h3>
         <h6>Select your name</h6>
         <div id="guest-select">
@@ -99,7 +181,9 @@ export default function VotingForm(props: gameNightProps) {
             name="guests"
             id="guest-list-dropdown"
             value={guest}
-            onChange={(event) => setGuest(event.target.value)}
+            onChange={(event) => {
+              setGuest(event.target.value);
+            }}
           >
             {guestList.map((name: any, i: any) => {
               return (
@@ -110,10 +194,22 @@ export default function VotingForm(props: gameNightProps) {
             })}
           </select>
         </div>
+        <div id="email-confirm">
+          <p>{emailError}</p>
+          <label htmlFor="invitee-email">
+            <h3>Please confirm your email:</h3>
+          </label>
+          <input
+            id="invitee-email"
+            type="text"
+            value={inviteeEmail}
+            onChange={(event) => setInviteeEmail(event.target.value)}
+          ></input>
+        </div>
         <div id="rsvp-form">
           <h3>Will you be attending?</h3>
-          <Button>Yes</Button>
-          <Button>No</Button>
+          <Button onClick={() => setAttending('True')}>Yes</Button>
+          <Button onClick={() => setAttending('False')}>No</Button>
         </div>
         <Button type="submit" id="guest-list-button">
           Submit
