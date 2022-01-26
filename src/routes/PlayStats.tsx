9a912696bee968165108497c6ba26eb0de4cb31c
TrentHurlbut <trent.hurlbut@gmail.com>
2022-01-26 10:27:36 -0800

    Charts up for three of the weekday statistics we are tracking.

--- src/routes/PlayStats.tsx
+++ src/routes/PlayStats.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import * as V from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryLabel } from 'victory';
+import { Box } from '@mui/material';
 
 interface statProps {
   user: string;
@@ -28,6 +29,39 @@ export default function PlayStats(props: statProps) {
           .then((result) => {
             console.log(result);
             setWeekdayStats(result.data.weekday_stats);
+            for (let [key, value] of Object.entries(
+              result.data.weekday_stats.avg_attend_ratio
+            )) {
+              setAvgAttendRatio((oldData) => [
+                ...oldData,
+                {
+                  day: key,
+                  vote: value,
+                },
+              ]);
+            }
+            for (let [key, value] of Object.entries(
+              result.data.weekday_stats.avg_overall_feedback
+            )) {
+              setAvgOverallFeedback((oldData) => [
+                ...oldData,
+                {
+                  day: key,
+                  vote: value,
+                },
+              ]);
+            }
+            for (let [key, value] of Object.entries(
+              result.data.weekday_stats.avg_player_num
+            )) {
+              setAvgPlayerNum((oldData) => [
+                ...oldData,
+                {
+                  day: key,
+                  vote: value,
+                },
+              ]);
+            }
           })
           .catch((error) => console.log(error));
       } catch (error) {
@@ -35,14 +69,88 @@ export default function PlayStats(props: statProps) {
       }
     };
     loadData();
-  }, []);
+  }, [props.token]);
 
   return (
     <div id="play-stats-page">
       <h1>Welcome to Game Master, {props.user}!</h1>
       <div id="most-played">
-        <div id="by-title">
-          <h3>Most Played Games</h3>
+        <div id="weekday-stats">
+          <h3>Weekday Stats</h3>
+          <div id="weekday-vis-container">
+            <Box>
+              {avgAttendRatio.length > 0 ? (
+                <VictoryChart
+                  width={600}
+                  height={500}
+                  padding={{ left: 150, top: 50, right: 50, bottom: 150 }}
+                >
+                  <VictoryAxis
+                    label="Average Attendance Ratio per Weekday for Game Nights"
+                    style={{
+                      axisLabel: { fontSize: 20, padding: 40 },
+                      ticks: { stroke: 'grey', size: 5 },
+                    }}
+                  />
+                  <VictoryAxis
+                    dependentAxis
+                    style={{ ticks: { stroke: 'grey', size: 5 } }}
+                  />
+                  <VictoryBar data={avgAttendRatio} x="day" y="vote" />
+                </VictoryChart>
+              ) : (
+                <h2>Loading...</h2>
+              )}
+            </Box>
+            <Box>
+              {avgOverallFeedback.length > 0 ? (
+                <VictoryChart
+                  width={600}
+                  height={500}
+                  padding={{ left: 150, top: 50, right: 50, bottom: 150 }}
+                >
+                  <VictoryAxis
+                    label="Average Overall Score for Gamenights per Weekday"
+                    style={{
+                      axisLabel: { fontSize: 20, padding: 40 },
+                      ticks: { stroke: 'grey', size: 5 },
+                    }}
+                  />
+                  <VictoryAxis
+                    dependentAxis
+                    style={{ ticks: { stroke: 'grey', size: 5 } }}
+                  />
+                  <VictoryBar data={avgOverallFeedback} x="day" y="vote" />
+                </VictoryChart>
+              ) : (
+                <h2>Loading...</h2>
+              )}
+            </Box>
+            <Box>
+              {avgPlayerNum.length > 0 ? (
+                <VictoryChart
+                  width={600}
+                  height={500}
+                  padding={{ left: 150, top: 50, right: 50, bottom: 150 }}
+                >
+                  <VictoryAxis
+                    label="Average Attendance per Weekday for Game Nights"
+                    style={{
+                      axisLabel: { fontSize: 20, padding: 40 },
+                      ticks: { stroke: 'grey', size: 5 },
+                    }}
+                  />
+                  <VictoryAxis
+                    dependentAxis
+                    style={{ ticks: { stroke: 'grey', size: 5 } }}
+                  />
+                  <VictoryBar data={avgPlayerNum} x="day" y="vote" />
+                </VictoryChart>
+              ) : (
+                <h2>Loading...</h2>
+              )}
+            </Box>
+          </div>
         </div>
         <div id="by-genre">
           <h3>Most Played Genres</h3>
