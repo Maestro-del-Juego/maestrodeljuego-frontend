2f4a4aaf33cd58154404ee8683c24b7dcc271241
James Moore <james816am@gmail.com>
2022-02-01 12:42:16 -0500

    Categories added to game popovers and info page

--- src/routes/GameInfoPage.tsx
+++ src/routes/GameInfoPage.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import WishlistButton from '../components/WishlistButton';
 import CollectionButton from '../components/CollectionButton';
+import Chip from '@mui/material/Chip';
+import Box from '@mui/material/Box';
 
 interface gameInfoProps {
   token: string;
@@ -19,6 +21,7 @@ export default function GameInfoPage(props: gameInfoProps) {
   const [imageUrl, setImageUrl] = useState('');
   const [wishlisted, setWishlisted] = useState(false);
   const [owned, setOwned] = useState(false);
+  const [categories, setCategories] = useState([])
 
   useEffect(() => {
     const gameUrl = `https://maestrodeljuego.herokuapp.com/games/${params.gameId}`;
@@ -39,6 +42,7 @@ export default function GameInfoPage(props: gameInfoProps) {
           setImageUrl(response.data.image);
           setWishlisted(response.data.wishlisted);
           setOwned(response.data.owned);
+          setCategories(response.data.categories);
           console.log(response.data);
         });
     } else {
@@ -58,12 +62,19 @@ export default function GameInfoPage(props: gameInfoProps) {
 
   return (
     <div className="game-info-card">
-      <h2>{gameTitle}</h2>
-      <h6>({pubYear})</h6>
+      <h2 className="info-page-title">{gameTitle}</h2>
+      <h4 className="info-page-pub-year">({pubYear})</h4>
       <img className="game-box-image" alt={gameTitle} src={imageUrl}></img>
       <p>
         {minPlayers} - {maxPlayers} Players | {playtime} Min Playtime
       </p>
+      <Box className="popover-categories" sx={{ marginBottom:2, marginTop:2 }}>
+            <>
+            {categories.map((category) => (
+                <Chip label={category} key={category} sx={{marginLeft:"2px", marginRight:"2px"}}/>
+            ))}
+            </>
+        </Box>
       <WishlistButton
         token={props.token}
         gameId={params.gameId!} // ! is non-null assertion operator
